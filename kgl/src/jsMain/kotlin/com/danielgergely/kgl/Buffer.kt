e07891ee21a4c0ce0269a95c72ef3eae84765be1
Dániel Gergely <gergelydaniel@gmail.com>
2019-08-03 18:01:22 +0200

    Fix JS buffer put arguments

--- kgl/src/jsMain/kotlin/com/danielgergely/kgl/Buffer.kt
+++ kgl/src/jsMain/kotlin/com/danielgergely/kgl/Buffer.kt
@@ -11,15 +11,13 @@ actual class Buffer(buffer: FloatArray) {
         pos += 1
     }
 
-    actual fun put(floatArray: FloatArray) = put(floatArray, -1)
+    actual fun put(floatArray: FloatArray) = put(floatArray, floatArray.size)
 
     actual fun put(floatArray: FloatArray, length: Int) {
-        val l = if (length == -1) floatArray.size else length
-
         val floatArray: dynamic = floatArray
 
-        buffer.set(floatArray.subarray(0, l), pos)
-        pos += l
+        buffer.set(floatArray.subarray(0, length), pos)
+        pos += length
     }
 
     actual fun position(pos: Int) {
