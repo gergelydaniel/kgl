ee54866841c839749ad076e8d70e09e0a8cce152
Daniel Gergely <gergelydaniel@gmail.com>
2022-07-07 15:17:20 +0200

    Bump Kotlin version to 1.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
         classpath("com.android.tools.build:gradle:7.0.4")
     }
 }
@@ -14,7 +14,7 @@ buildscript {
 val versionInfo = versionInfo()
 println("Version:${versionInfo.versionName}, Type:${versionInfo.type}")
 
-rootProject.ext["kotlin_version"] = "1.6.21"
+rootProject.ext["kotlin_version"] = "1.7.0"
 
 allprojects {
     repositories {
