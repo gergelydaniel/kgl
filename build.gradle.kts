f4f0d7b3707bf95f988675cf1b00dc70a2303407
Daniel Gergely <gergelydaniel@gmail.com>
2022-07-13 14:29:36 +0200

    Bump Kotlin version to 1.7.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
         classpath("com.android.tools.build:gradle:7.0.4")
     }
 }
@@ -14,7 +14,7 @@ buildscript {
 val versionInfo = versionInfo()
 println("Version:${versionInfo.versionName}, Type:${versionInfo.type}")
 
-rootProject.ext["kotlin_version"] = "1.7.0"
+rootProject.ext["kotlin_version"] = "1.7.10"
 
 allprojects {
     repositories {
